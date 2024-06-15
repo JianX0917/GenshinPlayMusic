5fcd73f9d3679394f9b0433026bcde5864274acd
jian <12792042+xue_jian917@user.noreply.gitee.com>
2024-06-15 21:51:57 +0800

    加入了部分异常处理

--- src/player.py
+++ src/player.py
@@ -17,6 +17,7 @@ class Player:
         self.music_name = None
         print("演奏者就绪")
 
+    # 加载乐谱
     def get_music(self, music):
         self._music = music.music
         self.speed = music.speed
@@ -24,24 +25,31 @@ class Player:
         self.music_name = music.name
         print("准备演奏: 《" + self.music_name + "》\n")
 
+    # 更新速度值
     def _update_speed(self, speed):
-        # TODO : if speed <= 0 : raise ValueError("非法速度值") 
+        if speed <= 0:
+            raise PlayerException("更新速度值 {} 非法".format(speed))
         self.speed = speed
         self._interval = 60 / speed
 
+    # 演奏乐曲
     def play(self):
-        # TODO : if not self._music or not self._interval :raise PlayerError("异常提示")
+        if self._music is None:
+            raise PlayerException("未获得乐谱,无法演奏")
+        if not self._interval:
+            raise PlayerException("未设置演奏速度,无法演奏")
         print("开始演奏: 《" + self.music_name + "》\n")
         for i in range(len(self._music)):
             row = self._music.iloc[i]
+            # 根据该行的标识符决定该行处理方式
             if "s" in row.iloc[0]:  # 变速
                 self._update_speed(row.iloc[2])
-                continue
-            if "a" in row.iloc[0]:  # 琶音
+            elif "a" in row.iloc[0]:  # 演奏琶音
                 self._play_arpeggio(row.iloc[1], row.iloc[2], row.iloc[3])
-                continue
-            self._play_one_row(row.iloc[1], row.iloc[2], row.iloc[3])
+            else:  # 正常演奏
+                self._play_one_row(row.iloc[1], row.iloc[2], row.iloc[3])
 
+    # 弹奏一行
     def _play_one_row(self, keys, pause, lyrics):
         if keys:
             pyautogui.hotkey(keys)  # 弹奏音符
@@ -50,28 +58,43 @@ class Player:
         if pause:
             pyautogui.sleep(pause * self._interval)  # 停顿
 
+    # 弹奏琶音
     def _play_arpeggio(self, keys, pause, lyrics):
         keys_num = len(keys)
         arpeggio_interval = min(0.02, pause * self._interval / keys_num)  # 0.02时自定义的琶音间隔
         pyautogui.typewrite(keys, interval=arpeggio_interval)
-        print(lyrics, end="", flush=True)  # 打印歌词
+        if not pd.isnull(lyrics):
+            print(lyrics, end="", flush=True)  # 打印歌词
         pyautogui.sleep(pause * self._interval - arpeggio_interval * keys_num)  # 剩余停顿
 
-    def move(self, direction, time):
+    # 控制人物走路
+    def walk(self, direction, time):
         direction_to_key = {"left": 'a', "right": 'd', "forward": 'w', "backward": 's'}
-        # TODO : if direction not in direction_to_key : raise PlayerError("移动方向非法")
-        # TODO : if time <= 0 : raise PlayerError("移动时长非法")
         pyautogui.press("ctrl")  # 切换为行走模式
-        pyautogui.keyDown(direction_to_key[direction])
-        pyautogui.sleep(time)
+        try:
+            pyautogui.keyDown(direction_to_key[direction])
+        except KeyError:
+            raise PlayerException("移动方向 {} 非法".format(direction))
+        try:
+            pyautogui.sleep(time)
+        except ValueError:
+            raise PlayerException("移动时长 {} 非法".format(time))
         pyautogui.keyUp(direction_to_key[direction])
         pyautogui.press("ctrl")  # 切换回跑步模式
 
+    # 打开风物之诗琴
     def start_play(self):
         pyautogui.press("z")
 
+    # 关闭风物之诗琴
     def stop_play(self):
         pyautogui.press("esc")
+
+
+# 异常类型
+class PlayerException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
 
 
 if __name__ == '__main__':
