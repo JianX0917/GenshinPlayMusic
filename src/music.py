52b00dab03362e798c232f0af4f874e3b797b698
jian <12792042+xue_jian917@user.noreply.gitee.com>
2024-06-15 10:58:44 +0800

    加入琶音代码，修改标识的解析方式

--- src/music.py
+++ src/music.py
@@ -6,6 +6,7 @@ Created on Sun May  5 23:07:02 2024
 
 import pandas as pd
 
+
 class Music:
     # 简谱至键盘谱转换表
     _dic = {"+1": "Q", "+2": "W", "+3": "E", "+4": "R", "+5": "T", "+6": "Y", "+7": "U",
@@ -17,7 +18,6 @@ class Music:
         column_names = ['标记', '音符', '间隔', '歌词']
         dtype = {'标记': str, '音符': str, '间隔': float, '歌词': str}
         self.music = pd.read_excel(file_path, index_col=False, header=0, names=column_names, dtype=dtype)
-        self.name = file_path.split('/')[-1].split('.')[0].replace('_', ' ')
         # 设置速度
         if self.music.empty or self.music.iloc[0, 0] != 's':
             raise ValueError("乐谱格式有误")
@@ -25,19 +25,54 @@ class Music:
             raise ValueError("乐谱速度有误")
         self.speed = self.music.iloc[0, 2]
         self.music = self.music.drop(0)
+        # 从Excel中读取乐名，若Excel中未设置乐名，则以Excel文件名作为默认乐名
+        self.name = self._get_name(file_path)
         # 简谱转换至键盘谱
         self._notation_conversion()
+        # TODO:使用日志打印错误信息  使用列表记录错误信息  若列表为空  则打印乐谱准备完成 否则记录错误信息
+        # TODO:记录已抛弃行数指针 用于打印错误信息时指明第几行错误
         print("乐谱准备完成")
 
+    def _get_name(self, file_path):
+        name = None
+        # 优先从Excel中读取乐名
+        if self.music.empty or self.music.iloc[0, 0] == 'n':
+            name = self.music.iloc[0, 1]
+            self.music = self.music.drop(1)
+        # 无法从Excel中读取到乐名，将Execl文件名作为默认乐名
+        if name is None:
+            name = file_path.split('/')[-1].split('.')[0].replace('_', ' ')
+        return name
+
+    # TODO:改成手动转换，不要自动一列一列的转换了 转换的时候用try-catch捕获异常最后仪器抛出
     def _notation_conversion(self):
+        self.music['标记'] = self.music['标记'].apply(self._split_identifier)
         self.music['音符'] = self.music['音符'].apply(self._number_notation_to_key_notation)
 
+    def _split_identifier(self, identifier):
+        if identifier:
+            return str(identifier).split(",")
+            
+
     def _number_notation_to_key_notation(self, number_notation):
+        # 自定义排序使得低音在高音的前面，这样弹奏琶音时才是从低到高
+        def sort_key(s):
+            if len(s) == 0:
+                return 0
+            if s[0] == '-':
+                return int(s[1])
+            elif s[0] == '+':
+                return 20 + int(s[1])
+            else:
+                return 10 + int(s)
         if pd.isnull(number_notation):
             return
         number_notation = number_notation.split(",")
+        number_notation = sorted(number_notation, key=sort_key)
         key_notation = []
         for musical_notes in number_notation:
+            if musical_notes == "0" or musical_notes == "":
+                continue
             key_notation.append(self._dic[musical_notes])
         return key_notation
 
--- src/player.py
+++ src/player.py
@@ -11,46 +11,67 @@ from music import Music
 
 class Player:
     def __init__(self):
+        self._interval = None
+        self.speed = None
+        self._music = None
+        self.music_name = None
         print("演奏者就绪")
 
     def get_music(self, music):
         self._music = music.music
-        self._speed = music.speed
-        self._interval = 60 / self._speed
-        print("准备演奏: 《" + music.name + "》\n")
+        self.speed = music.speed
+        self._interval = 60 / self.speed
+        self.music_name = music.name
+        print("准备演奏: 《" + self.music_name + "》\n")
 
     def _update_speed(self, speed):
+        # TODO : if speed <= 0 : raise ValueError("非法速度值") 
         self.speed = speed
-        self.interval = 60 / speed
+        self._interval = 60 / speed
 
     def play(self):
+        # TODO : if not self._music or not self._interval :raise PlayerError("异常提示")
+        print("开始演奏: 《" + self.music_name + "》\n")
         for i in range(len(self._music)):
             row = self._music.iloc[i]
-            if row.iloc[0] == "s":  # 变速
+            if "s" in row.iloc[0]:  # 变速
                 self._update_speed(row.iloc[2])
+                continue
+            if "a" in row.iloc[0]:  # 琶音
+                self._play_arpeggio(row.iloc[1], row.iloc[2], row.iloc[3])
                 continue
             self._play_one_row(row.iloc[1], row.iloc[2], row.iloc[3])
 
     def _play_one_row(self, keys, pause, lyrics):
-        if keys is None:
-            return
-        pyautogui.typewrite(keys)
-        pyautogui.sleep(pause * self._interval)
+        if keys:
+            pyautogui.hotkey(keys)  # 弹奏音符
         if not pd.isnull(lyrics):
-            print(lyrics, end="", flush=True)
+            print(lyrics, end="", flush=True)  # 打印歌词
+        if pause:
+            pyautogui.sleep(pause * self._interval)  # 停顿
+
+    def _play_arpeggio(self, keys, pause, lyrics):
+        keys_num = len(keys)
+        arpeggio_interval = min(0.02, pause * self._interval / keys_num)  # 0.02时自定义的琶音间隔
+        pyautogui.typewrite(keys, interval=arpeggio_interval)
+        print(lyrics, end="", flush=True)  # 打印歌词
+        pyautogui.sleep(pause * self._interval - arpeggio_interval * keys_num)  # 剩余停顿
 
     def move(self, direction, time):
         direction_to_key = {"left": 'a', "right": 'd', "forward": 'w', "backward": 's'}
-        pyautogui.press("ctrl")
+        # TODO : if direction not in direction_to_key : raise PlayerError("移动方向非法")
+        # TODO : if time <= 0 : raise PlayerError("移动时长非法")
+        pyautogui.press("ctrl")  # 切换为行走模式
         pyautogui.keyDown(direction_to_key[direction])
         pyautogui.sleep(time)
         pyautogui.keyUp(direction_to_key[direction])
+        pyautogui.press("ctrl")  # 切换回跑步模式
 
     def start_play(self):
         pyautogui.press("z")
 
     def stop_play(self):
-        pyautogui.press("z")
+        pyautogui.press("esc")
 
 
 if __name__ == '__main__':
@@ -60,5 +81,6 @@ if __name__ == '__main__':
     pyautogui.sleep(5)
     # player.move("backward", 3)
     player.start_play()
-    pyautogui.sleep(1)
+    pyautogui.sleep(2)
     player.play()
+    player.stop_play()
